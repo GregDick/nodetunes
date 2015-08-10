864893532b760d6559491afc051f7bf2127d59ef
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-10 15:23:21 -0500

    artists route connected to local mongodb

--- routes/artists.js
+++ routes/artists.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('templates/artists')
+  var collection = global.db.collection('nodetunes');
+  collection.find().toArray(function(err, artist){
+    var prettyArtists = artist.map(function(artist){
+      return{
+        name : artist.name,
+        genre: artist.genre,
+        origin: artist.origin || 'N/A'
+      }
+    })
+    res.render('templates/artists', {artists: prettyArtists});
+  })
 });
 
 
