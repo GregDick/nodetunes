46ab6fc40af5a58ea70b73f86ba7518c1eb5dd07
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-13 12:10:23 -0500

    added create album form and function

--- routes/albums.js
+++ routes/albums.js
@@ -6,8 +6,19 @@ router.get('/', function(req, res){
   res.render('templates/albums.ejs');
 });
 
+router.get('/albums/search', function(req, res){
 
+})
 
-
+router.post('/:id/new', function(req, res){
+  var collection = global.db.collection('albums');
+  collection.save(
+    {_id: ObjectID(req.params.id),
+    title: req.body.title,
+    year: req.body.year
+    }, function(err, response){
+      res.redirect('/artists/'+req.params.id);
+    })
+})
 
 module.exports = router;
