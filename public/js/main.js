490dc61bae122e500140bf98c3d320893cf02271
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-24 18:40:57 -0500

    added album artwork with spotify api

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,12 @@ $('.editAlbumButton').on('click', function(){
   $('.albumEdit').show();
 })
 
+if(window.location.pathname.split('albums')[1].length){
+  var $title = $('.albumTitle').text();
+  var $albumArt = $('.albumArt');
+  $.get('https://api.spotify.com/v1/search?type=album&q=' + $title, function(data){
+    var imgUrl = data.albums.items[0].images[0].url;
+    console.log(imgUrl);
+    $albumArt.append("<img class='pull-right' src='"+ imgUrl +"'>");
+  })
+}
